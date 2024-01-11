ef6e96503d28ea1d158cb479d05dbb62087eb7b5
harunn32 <ozkanha7@gmail.com>
2024-01-11 11:46:32 +0300

    rezervasyon

--- dosya.cpp
+++ dosya.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <fstream>
 #include <locale.h>
-
 using namespace std;
 
 // Müþteri bilgilerini tutan struct
@@ -17,22 +16,39 @@ void DosyayaYaz(ofstream& dosya, const Musteri& musteri) {
     dosya << musteri.isim << " " << musteri.soyisim << " " << musteri.cinsiyet << " " << musteri.odaNumarasi << endl;
 }
 
+// Dosyadan müþteri sayýsýný okuyan fonksiyon
+int DosyadanMusteriSayisiniOku(ifstream& dosya) {
+    int musteriSayisi = 0;
+    string satir;
+    while (getline(dosya, satir)) {
+        musteriSayisi++;
+    }
+    return musteriSayisi;
+}
+
 int main() {
     // Dosyayý aç
-     setlocale(LC_ALL, "Turkish");
-    ofstream dosya("musteri_bilgileri.txt", ios::app); // ios::app, dosyanýn sonuna ekleme yapmayý saðlar
+    setlocale (LC_ALL,"Turkish") ;
+    ifstream okumaDosyasi("musteri_bilgileri.txt");
+    ofstream yazmaDosyasi("musteri_bilgileri.txt", ios::app);
 
     // Dosya açýlýp açýlamadýðýný kontrol et
-    if (!dosya.is_open()) {
-        cout << "Dosya açma hatasi!" << endl;
+    if (!yazmaDosyasi.is_open() || !okumaDosyasi.is_open()) {
+        cout << "Dosya acma hatasi!" << endl;
         return 1;
     }
 
-    // Müþteri bilgilerini giriþi için switch-case yapýsý
+    // Baþlangýçta kayýtlý müþteri sayýsýný al
+    int kayitliMusteriSayisi = DosyadanMusteriSayisiniOku(okumaDosyasi);
+    cout << "Baslangicta Kayitli Musteri Sayisi: " << kayitliMusteriSayisi << endl;
+
+    // Ana menü
     int secim;
     do {
+        cout << "\n----- MENU -----\n";
         cout << "1. Rezervasyon yap\n";
-        cout << "2. Çýkýþ\n";
+        cout << "2. Musteri Sayisini Goruntule\n";
+        cout << "3. Cikis\n";
         cout << "Seciminizi yapin: ";
         cin >> secim;
 
@@ -49,21 +65,28 @@ int main() {
                 cin >> yeniMusteri.odaNumarasi;
 
                 // Dosyaya yaz
-                DosyayaYaz(dosya, yeniMusteri);
+                DosyayaYaz(yazmaDosyasi, yeniMusteri);
+
+                // Müþteri sayýsýný artýr
+                kayitliMusteriSayisi++;
 
                 cout << "Rezervasyon tamamlandi!\n";
                 break;
             }
             case 2:
+                cout << "Toplam Kayitli Musteri Sayisi: " << kayitliMusteriSayisi << endl;
+                break;
+            case 3:
                 cout << "Programdan cikiliyor.\n";
                 break;
             default:
-                cout << "Geçersiz secim! Tekrar deneyin.\n";
+                cout << "Gecersiz secim! Tekrar deneyin.\n";
         }
-    } while (secim != 2);
+    } while (secim != 3);
 
-    // Dosyayý kapat
-    dosya.close();
+    // Dosyalarý kapat
+    okumaDosyasi.close();
+    yazmaDosyasi.close();
 
     return 0;
 }
